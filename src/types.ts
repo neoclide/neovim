e21b44313374048d278f043047cfbc5548f17797
Qiming Zhao <chemzqm@gmail.com>
2020-12-30 17:11:58 +0800

    fix Logger interface

--- src/types.ts
+++ src/types.ts
@@ -68,11 +68,9 @@ export type VimValue =
   | { [key: string]: any }
 
 export interface Logger {
-  trace(message: any, ...args: any[]): void
   debug(message: any, ...args: any[]): void
   info(message: any, ...args: any[]): void
   warn(message: any, ...args: any[]): void
   error(message: any, ...args: any[]): void
-  mark(message: any, ...args: any[]): void
 }
 
